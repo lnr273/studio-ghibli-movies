e0f261239c241b6b6b5fa1d2435743f2b982a48d
Luan Reis <luanreis2723@gmail.com>
2024-10-12 09:47:33 -0300

    remoções

--- frontend/src/components/Card/index.jsx
+++ frontend/src/components/Card/index.jsx
@@ -1,11 +1,6 @@
 /* eslint-disable react/prop-types */
 import { Link } from 'react-router-dom';
 import styles from './Card.module.css'
-import whiteHeart from '../../assets/white-heart.svg'
-import redHeart from '../../assets/red-heart.svg'
-import getCookie from "../../hooks/getCookie.js"
-import { jwtDecode } from "jwt-decode"
-import { useEffect, useState } from 'react';
 
 function Card({movie}) {
     return (
